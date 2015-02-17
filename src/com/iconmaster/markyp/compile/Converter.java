1fe63f269f63b130cca42df9a47c3098a726a008
Matthew Messinger <mattmess1221@gmail.com>
2015-02-17 16:32:56 -0500

    Modularize Converter so it's easier to read and doesn't extend too far to the right. Still needs proper comments, though. TODO: Start thinking with objects

--- src/com/iconmaster/markyp/compile/Converter.java
+++ src/com/iconmaster/markyp/compile/Converter.java
@@ -8,207 +8,243 @@ import java.util.Stack;
  * @author iconmaster
  */
 public class Converter {
+
 	public int width = 19;
 	public int height = 13;
 	public int tabSize = 1;
 
-	public String toJSON(Formatter.Output format) { // TODO Modularize
-		StringBuilder sb = new StringBuilder("[\"");
-		StringBuilder line = new StringBuilder();
-		int col = 0;
-		int row = 0;
+	/**
+	 * Used to pass variables through methods. Reassigned each run.
+	 */
+	private ConverterData data;
+
+	public String toJSON(Formatter.Output format) {
+		data = new ConverterData(format);
 		boolean bullet = true;
-		boolean brk = false;
-		Stack<String> alignMode = new Stack<String>();
-		Stack<ListMode> listMode = new Stack<ListMode>();
-		alignMode.push("left");
+		StringBuilder sb = new StringBuilder("[\"");
+		data.alignMode.push("left");
 
-		for (int i = 0; i < format.s.length(); i++) {
-			char c = format.s.charAt(i);
-			char op = format.c.charAt(i);
+		for (int i = 0; i < data.format.s.length(); i++) {
 
-			switch (op) {
-				case (Constants.OP_TEXT):
-					line.append(c);
-					col++;
-					break;
-				case (Constants.OP_BEGIN):
-					switch (c) {
-						case (Constants.CHAR_BOLD):
-							line.append("\",{text:\"\",bold:true,extra:[\"");
-							break;
-						case (Constants.CHAR_ITALIC):
-							line.append("\",{text:\"\",italic:true,extra:[\"");
-							break;
-						case (Constants.CHAR_UNDERLINE):
-							line.append("\",{text:\"\",underline:true,extra:[\"");
-							break;
-						case (Constants.CHAR_OBF):
-							line.append("\",{text:\"\",obfuscated:true,extra:[\"");
-							break;
-						case (Constants.CHAR_STRIKE):
-							line.append("\",{text:\"\",strikethrough:true,extra:[\"");
-							break;
-						case (Constants.CHAR_ALIGN):
-							alignMode.push((String) format.argMap.get(i));
-							break;
-						case (Constants.CHAR_COLOR):
-							line.append("\",{text:\"\",color:\"");
-							line.append((String) format.argMap.get(i));
-							line.append("\",extra:[\"");
-							break;
-						case (Constants.CHAR_SEL):
-							line.append("\",{selector:\"");
-							break;
-						case (Constants.CHAR_CLICK):
-							String[] ss = (String[]) format.argMap.get(i);
-							line.append("\",{text:\"\",clickEvent:{action:\"");
-							line.append(ss[0]);
-							line.append("\",value:\"");
-							line.append(ss[1]);
-							line.append("\"},extra:[\"");
-							break;
-						case (Constants.CHAR_HOVER):
-							ss = (String[]) format.argMap.get(i);
-							line.append("\",{text:\"\",hoverEvent:{action:\"");
-							line.append(ss[0]);
-							line.append("\",value:\"");
-							line.append(ss[1]);
-							line.append("\"},extra:[\"");
-							break;
-						case (Constants.CHAR_LIST):
-							listMode.push((ListMode) format.argMap.get(i));
-							break;
-					}
-					break;
-				case (Constants.OP_END):
-					switch (c) {
-						case (Constants.CHAR_BOLD):
-						case (Constants.CHAR_ITALIC):
-						case (Constants.CHAR_UNDERLINE):
-						case (Constants.CHAR_OBF):
-						case (Constants.CHAR_STRIKE):
-						case (Constants.CHAR_COLOR):
-						case (Constants.CHAR_CLICK):
-						case (Constants.CHAR_HOVER):
-							line.append("\"]},\"");
-							break;
-						case (Constants.CHAR_SEL):
-							line.append("\"},\"");
-							break;
-						case (Constants.CHAR_ALIGN):
-							alignMode.pop();
-							break;
-						case (Constants.CHAR_LIST):
-							listMode.pop();
-							break;
-					}
-					break;
-				case (Constants.OP_EXEC):
-					switch (c) {
-						case (Constants.CHAR_BR):
-							brk = true;
-							break;
-						case (Constants.CHAR_PBR):
-							brk = true;
-							row = height-1;
-							break;
-						case (Constants.CHAR_SCORE): {
-							String[] args = (String[]) format.argMap.get(i);
+			parse(i);
 
-							line.append("\",{score:{name:\"");
-							line.append(args[0]);
-							line.append("\",objective:\"");
-							line.append(args[1]);
-							line.append("\"}},\"");
-						}
-						break;
-						case (Constants.CHAR_LANG): {
-							ArrayList<String> args = (ArrayList<String>) format.argMap.get(i);
-							line.append("\",{translate:\"");
-							line.append(args.get(0));
-							line.append("\"");
-							if (args.size()>1) {
-								line.append(",with:[");
-								for (int j=1;j<args.size();j++) {
-									line.append("\"");
-									line.append(args.get(j));
-									line.append("\",");
-								}
-								line.deleteCharAt(line.length()-1);
-								line.append("]");
-							}
-							line.append("},\"");
-						}
-						break;
-						case (Constants.CHAR_TABSIZE):
-							this.tabSize = (Integer) format.argMap.get(i);
-							break;
-						case (Constants.CHAR_TAB):
-							int tabs = tabSize;
-							if (format.argMap.containsKey(i)) {
-								tabs = (Integer) format.argMap.get(i);
-							}
-							for (int j=0;j<tabs;j++) {
-								line.append(" ");
-								col++;
-							}
-							break;
-					}
-					break;
-			}
-
-			if (col==width) {
-				brk = true;
+			if (data.col == width) {
+				data.brk = true;
 				bullet = false;
 			}
 
-			if (brk) {
-				row++;
-				if (alignMode.peek().equalsIgnoreCase("left")) {
-					for (int j=col;j<width;j++) {
-						line.append(" ");
+			if (data.brk) {
+				data.row++;
+				if (data.alignMode.peek().equalsIgnoreCase("left")) {
+					for (int j = data.col; j < width; j++) {
+						data.line.append(" ");
 					}
-				} else if (alignMode.peek().equalsIgnoreCase("right")) {
-					for (int j=width;j>col;j--) {
+				} else if (data.alignMode.peek().equalsIgnoreCase("right")) {
+					for (int j = width; j > data.col; j--) {
 						sb.append(" ");
 					}
-				} else if (alignMode.peek().equalsIgnoreCase("center")) {
-					for (int j=width;j>col;j-=2) {
+				} else if (data.alignMode.peek().equalsIgnoreCase("center")) {
+					for (int j = width; j > data.col; j -= 2) {
 						sb.append(" ");
 					}
-					for (int j=col;j<width;j+=2) {
-						line.append(" ");
+					for (int j = data.col; j < width; j += 2) {
+						data.line.append(" ");
 					}
 				}
 
-				if (bullet && !listMode.isEmpty()) {
-					int tabs = tabSize * listMode.size();
-					ListMode lm = listMode.peek();
+				if (bullet && !data.listMode.isEmpty()) {
+					int tabs = tabSize * data.listMode.size();
+					ListMode lm = data.listMode.peek();
 					String prefix = lm.nextPoint();
-					for (int j=0;j<tabs;j++) {
+					for (int j = 0; j < tabs; j++) {
 						sb.append(" ");
 					}
 					sb.append(prefix);
 					sb.append(" ");
-					col+=prefix.length()+tabs+1;
+					data.col += prefix.length() + tabs + 1;
 				}
 
-				sb.append(line);
-				if (row==height) {
-					row = 0;
+				sb.append(data.line);
+				if (data.row == height) {
+					data.row = 0;
 					sb.append("\",/*@PBR@*/\"");
 				}
-				line = new StringBuilder();
-				col = 0;
+				data.line = new StringBuilder();
+				data.col = 0;
 
-				brk = false;
+				data.brk = false;
 				bullet = true;
 			}
 		}
 
-		sb.append(line);
+		sb.append(data.line);
 		sb.append("\"]");
 		return sb.toString();
+	}
+
+	private void parse(int i) {
+		char c = data.format.s.charAt(i);
+		char op = data.format.c.charAt(i);
+
+		switch (op) {
+		case (Constants.OP_TEXT):
+			data.line.append(c);
+			data.col++;
+			break;
+		case (Constants.OP_BEGIN):
+			parseBegin(i, c);
+			break;
+		case (Constants.OP_END):
+			parseEnd(c);
+			break;
+		case (Constants.OP_EXEC):
+			parseExec(i, c);
+			break;
+		}
+	}
+
+	private void parseBegin(int i, char c) {
+		switch (c) {
+		case (Constants.CHAR_BOLD):
+			data.line.append("\",{text:\"\",bold:true,extra:[\"");
+			break;
+		case (Constants.CHAR_ITALIC):
+			data.line.append("\",{text:\"\",italic:true,extra:[\"");
+			break;
+		case (Constants.CHAR_UNDERLINE):
+			data.line.append("\",{text:\"\",underline:true,extra:[\"");
+			break;
+		case (Constants.CHAR_OBF):
+			data.line.append("\",{text:\"\",obfuscated:true,extra:[\"");
+			break;
+		case (Constants.CHAR_STRIKE):
+			data.line.append("\",{text:\"\",strikethrough:true,extra:[\"");
+			break;
+		case (Constants.CHAR_ALIGN):
+			data.alignMode.push((String) data.format.argMap.get(i));
+			break;
+		case (Constants.CHAR_COLOR):
+			data.line.append("\",{text:\"\",color:\"");
+			data.line.append((String) data.format.argMap.get(i));
+			data.line.append("\",extra:[\"");
+			break;
+		case (Constants.CHAR_SEL):
+			data.line.append("\",{selector:\"");
+			break;
+		case (Constants.CHAR_CLICK):
+			String[] ss = (String[]) data.format.argMap.get(i);
+			data.line.append("\",{text:\"\",clickEvent:{action:\"");
+			data.line.append(ss[0]);
+			data.line.append("\",value:\"");
+			data.line.append(ss[1]);
+			data.line.append("\"},extra:[\"");
+			break;
+		case (Constants.CHAR_HOVER):
+			ss = (String[]) data.format.argMap.get(i);
+			data.line.append("\",{text:\"\",hoverEvent:{action:\"");
+			data.line.append(ss[0]);
+			data.line.append("\",value:\"");
+			data.line.append(ss[1]);
+			data.line.append("\"},extra:[\"");
+			break;
+		case (Constants.CHAR_LIST):
+			data.listMode.push((ListMode) data.format.argMap.get(i));
+			break;
+		}
+	}
+
+	private void parseEnd(char c) {
+		switch (c) {
+		case (Constants.CHAR_BOLD):
+		case (Constants.CHAR_ITALIC):
+		case (Constants.CHAR_UNDERLINE):
+		case (Constants.CHAR_OBF):
+		case (Constants.CHAR_STRIKE):
+		case (Constants.CHAR_COLOR):
+		case (Constants.CHAR_CLICK):
+		case (Constants.CHAR_HOVER):
+			data.line.append("\"]},\"");
+			break;
+		case (Constants.CHAR_SEL):
+			data.line.append("\"},\"");
+			break;
+		case (Constants.CHAR_ALIGN):
+			data.alignMode.pop();
+			break;
+		case (Constants.CHAR_LIST):
+			data.listMode.pop();
+			break;
+		}
+	}
+
+	private void parseExec(int i, char c) {
+		switch (c) {
+		case (Constants.CHAR_BR):
+			data.brk = true;
+			break;
+		case (Constants.CHAR_PBR):
+			data.brk = true;
+			data.row = height - 1;
+			break;
+		case (Constants.CHAR_SCORE): {
+			String[] args = (String[]) data.format.argMap.get(i);
+
+			data.line.append("\",{score:{name:\"");
+			data.line.append(args[0]);
+			data.line.append("\",objective:\"");
+			data.line.append(args[1]);
+			data.line.append("\"}},\"");
+		}
+			break;
+		case (Constants.CHAR_LANG): {
+			@SuppressWarnings("unchecked")
+			ArrayList<String> args = (ArrayList<String>) data.format.argMap
+					.get(i);
+			data.line.append("\",{translate:\"");
+			data.line.append(args.get(0));
+			data.line.append("\"");
+			if (args.size() > 1) {
+				data.line.append(",with:[");
+				for (int j = 1; j < args.size(); j++) {
+					data.line.append("\"");
+					data.line.append(args.get(j));
+					data.line.append("\",");
+				}
+				data.line.deleteCharAt(data.line.length() - 1);
+				data.line.append("]");
+			}
+			data.line.append("},\"");
+		}
+			break;
+		case (Constants.CHAR_TABSIZE):
+			this.tabSize = (Integer) data.format.argMap.get(i);
+			break;
+		case (Constants.CHAR_TAB):
+			int tabs = tabSize;
+			if (data.format.argMap.containsKey(i)) {
+				tabs = (Integer) data.format.argMap.get(i);
+			}
+			for (int j = 0; j < tabs; j++) {
+				data.line.append(" ");
+				data.col++;
+			}
+			break;
+		}
+	}
+
+	private class ConverterData {
+
+		Formatter.Output format;
+
+		int col = 0;
+		int row = 0;
+		boolean brk = false;
+		Stack<ListMode> listMode = new Stack<ListMode>();
+		Stack<String> alignMode = new Stack<String>();
+		StringBuilder line = new StringBuilder();
+
+		ConverterData(Formatter.Output format) {
+			this.format = format;
+		}
 	}
 }
